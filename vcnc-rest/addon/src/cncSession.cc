a1f94fb6241a8a069af213c861d4158c23c33fc1
Nick Okasinski <nicko7i@users.noreply.github.com>
2016-12-27 14:39:31 -0800

    Removed a reference to a deprecated header file

--- vcnc-rest/addon/src/cncSession.cc
+++ vcnc-rest/addon/src/cncSession.cc
@@ -12,7 +12,6 @@
 #include <prtcl/core/pcSessionExport.H>
 #include <prtcl/cnctrq/cnctrqClient.H>
 #include <prtcl/cnctrq/cnctrqCncSession.H>
-#include <prtcl/cnctrq/cnctrqProtocolsClient.H>
 #include <sstream>
 
 //
